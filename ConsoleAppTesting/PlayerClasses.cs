26f62a1cefef4e349424aef902334834172a8a74
ivarmar <141323294+ivarmar@users.noreply.github.com>
2023-11-13 13:55:22 +0100

    Add project files.

---
+++ ConsoleAppTesting/PlayerClasses.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTesting
+{
+    internal class PlayerClasses
+    {
+        private List<string> classes = new List<string>();
+        public PlayerClasses() {
+            classes.Add("Warrior");
+            classes.Add("Priest");
+        }
+        public void DisplayClasses() {
+            Console.WriteLine("Choose one class:");
+            foreach (string playerClass in classes)
+            {
+            Console.WriteLine(playerClass);
+            }
+    }
+        public string ChooseClass()
+        {
+            Console.Write("Well what are you? ");
+            string chosenClass = Console.ReadLine();
+
+            if (classes.Contains(chosenClass))
+            {
+                Console.WriteLine($"You have chosen {chosenClass}!");
+                return chosenClass;
+            }
+            else
+            {
+                Console.WriteLine("Invalid class choice. Please choose from the available classes.");
+                return null;
+            }
+        }
+    }
+}
---
+++ ConsoleAppTesting/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleAppTesting;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your name?");
+        var nameInput = Console.ReadLine();
+        Console.WriteLine($"hello there, {nameInput}");
+        Console.WriteLine("What class do you want to be proficient in?");
+
+        PlayerClasses playerClasses = new PlayerClasses();
+        playerClasses.DisplayClasses();
+        string chosenClass = playerClasses.ChooseClass();
+        Console.WriteLine($"well I guess I should Welcome you, {nameInput} I see you are a novice {chosenClass} go to the guild over there and register proper.");
+
+        
+    }
+}
